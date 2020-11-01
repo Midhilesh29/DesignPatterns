2ac5fedf5cbb46c871a7082c45590300e633cfd8
Midhilesh29 <midhileshgithub@gmail.com>
2020-11-01 18:39:04 +0530

    Builder code

---
+++ Builder/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Person person1 = new Person.PersonBuilder().setFirstName("Midhilesh")
+                .setLastName("Elavazhagan")
+                .setPhoneNumber("1111111111")
+                .build();
+    }
+}
---
+++ Builder/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+    private String DisplayName;
+    private String FirstName;
+    private String LastName;
+    private int PhoneNumber;
+    private String status;
+    private Message DisplayPicture;
+    private Person(PersonBuilder builder){
+        this.DisplayName = builder.DisplayName;
+        this.FirstName = builder.FirstName;
+        this.LastName = builder.LastName;
+        this.PhoneNumber = builder.PhoneNumber;
+        this.status = builder.status;
+        this.DisplayPicture = builder.DisplayPicture;
+    }
+    public static class PersonBuilder{
+        private String DisplayName;
+        private String FirstName;
+        private String LastName;
+        private int PhoneNumber;
+        private String status;
+        public Person build(){
+            return new.Person(this)
+        }
+        public PersonBuilder setDisplayName(String dname){
+            this.DisplayName = dname;
+            return this;
+        }
+        public PersonBuilder setFirstName(String fname){
+            this.FirstName = fname;
+            return this;
+        }
+        public PersonBuilder setLastName(String lname){
+            this.LastName = lname;
+            return this;
+        }
+        public PersonBuilder setPhoneNumber(int number){
+            this.PhoneNumber = number;
+            return this;
+        }
+        public PersonBuilder setStatus(String status){
+            this.status = status;
+        }
+    }
+}
